8c48ecc5405b9b6620471397cde05bac8dc2276e
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-16 20:32:57 -0300

    Modulo User: Validations model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
+=begin
+  user_id:integer
+  first_name:string
+  last_name:string
+  institution:string
+=end
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true 
+  validates :last_name, presence: true 
+  validates :instituition, presence: true
+
+  enum role: { teacher: 0, student: 1 }
 end
