b482e1aa5ddb4ca5d07b673ca65893bafc78ae4a
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-17 22:50:37 -0300

    Module User: Sign_up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    confirmations: 'confirmations' 
+  }
+  
   get '/home', to: 'landing_page#index'
 
 
