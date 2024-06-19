72c5ce9dc465fe10ad4a354e35caa97d3d8e4e18
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-19 14:23:25 -0300

    Module Evaluation: Creation of model StatusEvaluation

---
+++ app/models/status_evaluation.rb
@@ -0,0 +1,2 @@
+class StatusEvaluation < ApplicationRecord
+end
---
+++ spec/factories/status_evaluations.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :status_evaluation do
+    name { "MyString" }
+    description { "MyString" }
+  end
+end
