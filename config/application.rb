abf130629ec6a03456d51a980fa877c4282b3e51
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-06-25 20:06:31 -0300

    Fix: Rename project

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "devise"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module OnlineTest
+module SmartExaminer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
