90816d9054fd8418837d482c627e7d8313ba22a3
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-07-09 16:25:16 -0300

    Feature: Change diagram

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,5 +11,4 @@ class Question < ApplicationRecord
 
   validates :question, presence: true
   validates :points, numericality: { greater_than_or_equal_to: 0 }
-  validates :answers, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :question do
     question { Faker::Lorem.sentence }
     feedback { Faker::Lorem.paragraph }
-    type { 1 }
+    type { (0..4).to_a.sample }
     points { Faker::Number.between(from: 0, to: 4) }
     correct_answer { Faker::Lorem.sentence }
     alternatives { Faker::Lorem.sentence }
