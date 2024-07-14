8a8aad31399953fbf4b4e6facc3ba348628b1da2
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-07-14 13:16:47 -0300

    Feature: Editing views of exams

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,28 +1,63 @@
 class QuestionsController < ApplicationController
   before_action :get_exam
-  before_action :get_question, only: [:show, :edit, :update, :destroy]
+  before_action :get_question, only: [:edit, :update, :destroy]
 
   def index
     @questions = @exam.questions
   end
 
   def new
+    @question = @exam.questions.build
   end
 
   def create
+    @question = @exam.questions.build(question_params)
+
+    respond_to do |format|
+     if @post.save  
+        flash.now[:success] = "Question created"
+        format.html { redirect_to shark_posts_path(@shark), notice: 'Post was successfully created.' }
+     else
+      flash.now[:error] = @question.errors.full_messages
+      format.html { render :new, status: :unprocessable_entity }
+  end
+end
   end
 
   def edit
   end
 
   def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.html { redirect_to exam_questions_url, notice: "Question was successfully updated." }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        flash.now[:error] = @question.errors.full_messages
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @question.destroy
+
+    respond_to do |format|
+      format.html { redirect_to exam_questions_url, notice: "Question was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
   def get_exam
     @exam = Exam.find(params[:exam_id])
+  end
+
+  def get_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:feedback, :alternatives,:points,:type_question, :question, :correct_answer, :type_question)
   end
 end
--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Questions", type: :request do
+  let!(:exam){create(:exam)}
+  let!(:question){create(:question, exam: exam)}
+
   describe "GET /index" do
     it "returns http success" do
-      get "/questions/index"
+      question
+      get exam_questions_path(question.exam.id)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
-      get "/questions/new"
+      get new_exam_question_path(exam.id)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
     it "returns http success" do
-      get "/questions/create"
+      get exam_questions_path(exam.id)
       expect(response).to have_http_status(:success)
     end
   end
