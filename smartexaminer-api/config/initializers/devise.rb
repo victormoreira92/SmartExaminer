6f67cf34dd754305e5b4edc270657d928a849a46
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-09-24 18:46:07 -0300

    build: Add configuration to initializer

--- smartexaminer-api/config/initializers/devise.rb
+++ smartexaminer-api/config/initializers/devise.rb
@@ -9,6 +9,10 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.api.configure do |api|
+    api.refresh_token.expires_in = 1.day
+    api.sign_up.extra_fields = [:name]
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
