ea4e94e6708717259a5d4a7101b2f165b8110e4a
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-09-29 15:26:24 -0300

    feat: Creating Quiz model

---
+++ smartexaminer-api/app/models/quiz.rb
@@ -0,0 +1,3 @@
+class Quiz < ApplicationRecord
+  valida
+end
---
+++ smartexaminer-api/spec/factories/quizzes.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :quiz do
+    title { "MyString" }
+    status_quiz { 1 }
+    description { "MyString" }
+  end
+end
