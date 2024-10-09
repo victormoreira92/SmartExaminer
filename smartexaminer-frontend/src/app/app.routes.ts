c95f4795cbaaff5371f71902ed99ff8d2c68b937
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-10-09 15:44:58 -0300

    refactor: Refactor of routes

--- smartexaminer-frontend/src/app/app.routes.ts
+++ smartexaminer-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeIndexComponent } from './components/home-index/home-index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch:'full'
+  },
+  {
+    component: HomeIndexComponent,
+    path: 'home'
+  }
+];
