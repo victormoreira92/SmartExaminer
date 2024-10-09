c033678400b7f079141e33ed781f67dca2b52824
Victor Hugo Moreira <vhmrj92@gmail.com>
2024-10-09 15:42:36 -0300

    feat: Creation of HomeIndexComponent

---
+++ smartexaminer-frontend/src/app/components/home-index/home-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-index.component.html',
+  styleUrl: './home-index.component.scss'
+})
+export class HomeIndexComponent {
+
+}
