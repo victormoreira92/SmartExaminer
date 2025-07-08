a460492cd10078c6abbf2f5b41ec504a4d1c6f37
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-07-08 15:31:46 -0300

    feat(front) : Criação das sections

--- smartexaminer-frontend/app/home/header.tsx
+++ smartexaminer-frontend/app/home/header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 export default function Header(){
   return (
      <header className="w-screen">
@@ -19,8 +20,8 @@ export default function Header(){
                 <a className="text-green-600 hover:text-green-800 px-3" href="#contato">Contato</a>
               </li>
             </ul>
-            <button className="bg-green-600 text-amber-100 text-1xl rounded-lg px-2">
-              Entrar
+            <button  className="bg-green-600 text-amber-100 text-1xl rounded-lg px-2">
+              <Link to="/dashboard">Entrar</Link>
             </button>
           </nav>
         </header>
--- smartexaminer-frontend/app/home/servicos.tsx
+++ smartexaminer-frontend/app/home/servicos.tsx
@@ -1,17 +1,50 @@
+import creationTest from './assets/creationTest.jpg'
+import provaImagem from './assets/prova.jpg'
+import sessionImagem from './assets/correction.png'
 export default function Servicos(){
   return (
-    <section className="flex justify-center" id="servicos">
-          <div>
+    <section className="flex justify-around text-blue-300 p-6" id="servicos">
+      <div className="flex flex-col justify-center w-3xs px-4">
+        <img 
+          src={creationTest}
+          alt='Drawing of creation test'
+        />
+        <h4 className='text-2xl'>
+          Monte provas personalizadas com facilidade
+        </h4>
+        <p className='text-base/6 py-2'>
+          Adicione questões com diferentes tipos de resposta, 
+          defina pontuação, tempo de execução e publique em poucos cliques.
+        </p>
+      </div>
 
-          </div>
+      <div className="flex flex-col justify-center w-3xs px-4">
+        <img 
+          src={provaImagem}
+          alt='Drawing of test'
+        />
+        <h4 className='text-2xl'>
+          Avaliação instantânea e precisa
+        </h4>
+        <p className='text-base/6 py-2'>
+          Adicione questões com diferentes tipos de resposta, 
+          defina pontuação, tempo de execução e publique em poucos cliques.
+        </p>
+      </div>
 
-          <div>
-
-          </div>
-
-          <div>
-            
-          </div>
+      <div className="flex flex-col justify-center w-3xs px-4">
+        <img 
+          src={creationTest}
+          alt='Drawing of test'
+        />
+       <h4 className='text-2xl'>
+          Aplicação de provas com segurança e integridade
+        </h4>
+        <p className='text-base/6 py-2'>
+          Evite fraudes com recursos de controle de tempo, 
+          bloqueio de abas e monitoramento inteligente durante os testes.
+        </p>
+      </div>
     </section>
   )
 }
