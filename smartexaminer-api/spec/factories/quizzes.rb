59f7f4a2fcd21d6ccd6b8b5eab222de21cf65798
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-07-09 11:47:16 -0300

    fix: Renomear de quiz para test

--- smartexaminer-api/spec/factories/quizzes.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :quiz do
-    title { Faker::Lorem.word }
-    status_quiz { 0 }
-    description { Faker::Lorem.sentence(word_count: 20) }
-  end
-end
--- smartexaminer-frontend/app/types/Quiz.ts
+++
@@ -1,7 +0,0 @@
-export interface Quiz {
-  id: number,
-  title: string,
-  description: Text,
-  created_at: Date,
-  updated_at: Date
-}
