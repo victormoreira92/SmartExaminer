d0d6db7547e5b7c4456298d75da4a1130c9c3d65
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-07-18 16:36:43 -0300

    feat: Style TrueFalse Component

--- smartexaminer-frontend/app/components/question/typeAnswers/TrueFalse.tsx
+++ smartexaminer-frontend/app/components/question/typeAnswers/TrueFalse.tsx
@@ -1,7 +1,23 @@
+import { Checkbox, CheckIcon, Label, Radio, TextInput } from "flowbite-react"
+
 export default function TrueFalse(){
   return (
-    <>
-      TrueFalse
-    </>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-2">
+        <Radio value="true" name="alternative" id="true" />
+        <Label htmlFor="accept" className="flex">
+          Set as correct
+        </Label>
+      </div>
+      <TextInput className="pb-2" type="text" id="disabledInput1" />
+
+      <div className="flex items-center gap-2">
+        <Radio value="false" name="alternative" id="fals" />
+        <Label htmlFor="accept" className="flex">
+          Set as correct
+        </Label>
+      </div>
+      <TextInput type="text" id="disabledInput1" />
+    </div>
   )
 }
