65680161eb22a7d4fbac79940295a55cb8571e12
Victor Hugo Moreira <vhmrj92@gmail.com>
2025-07-31 21:35:02 -0300

    config: Test vitest

--- smartexaminer-frontend/vite.config.ts
+++ smartexaminer-frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
 import tsconfigPaths from "vite-tsconfig-paths";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), flowbiteReact()],
+  plugins: [tailwindcss(), !process.env.VITEST && reactRouter(), tsconfigPaths(), flowbiteReact()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    css: true,
+  },
 });
